86124f3dcb6b858a9fb91101a810e9ddef607f1e
Valentine <valentine9304@gmail.com>
2025-04-01 17:10:55 +0700

    Add files via upload

--- api/db.py
+++ api/db.py
@@ -12,31 +12,34 @@ Base = declarative_base()
 engine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
+if "pytest" not in sys.modules:
+    producer = Producer({
+        "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS,
+        "transactional.id": "todo-producer",
+        "enable.idempotence": True,
+    })
+    producer.init_transactions()
 
-producer = Producer({
-    "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS,
-    "transactional.id": "todo-producer",
-    "enable.idempotence": True,
-})
-producer.init_transactions()
+    admin_client = AdminClient({
+        "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS
+    })
+    new_topic = NewTopic(topic=KAFKA_TOPIC, num_partitions=1, replication_factor=1)
+    fs = admin_client.create_topics([new_topic])
 
-admin_client = AdminClient({
-    "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS
-})
-new_topic = NewTopic(topic=KAFKA_TOPIC, num_partitions=1, replication_factor=1)
-fs = admin_client.create_topics([new_topic])
+    for topic, f in fs.items():
+        try:
+            f.result()
+            print(f"Топик '{topic}' создан.")
+        except Exception as e:
+            print(f"Не удалось создать топик '{topic}': {e}")
 
-for topic, f in fs.items():
-    try:
-        f.result()
-        print(f"Топик '{topic}' создан.")
-    except Exception as e:
-        print(f"Не удалось создать топик '{topic}': {e}")
-
-consumer = Consumer({
-    "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS,
-    "group.id": "notification_worker_group",
-    "auto.offset.reset": "earliest",
-    "enable.auto.commit": False
-})
-consumer.subscribe([KAFKA_TOPIC])
+    consumer = Consumer({
+        "bootstrap.servers": KAFKA_BOOTSTRAP_SERVERS,
+        "group.id": "notification_worker_group",
+        "auto.offset.reset": "earliest",
+        "enable.auto.commit": False
+    })
+    consumer.subscribe([KAFKA_TOPIC])
+else:
+    consumer = None
+    producer = None
